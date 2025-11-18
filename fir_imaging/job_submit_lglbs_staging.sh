be8b213c732bde8d0230ef5b03f54708f0644930
Eric Koch <koch.eric.w@gmail.com>
2025-11-18 06:25:23 -0500

    Add scripts for jobs running on fir

---
+++ fir_imaging/job_submit_lglbs_staging.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+#SBATCH --time=72:00:00
+#SBATCH --mem=256G
+#SBATCH --cpus-per-task=16
+#SBATCH --job-name=lglbs-staging-%J
+#SBATCH --output=lglbs-staging-%J.out
+#SBATCH --mail-user=ekoch@ualberta.ca
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+
+
+# For now, just pass the target name from the cmd line with sbatch.
+# Will want the ability to pass an array of target names later.
+export target_name=$1
+
+
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+
+
+module load StdEnv
+module load qt
+
+source /home/ekoch/.bashrc
+
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/usr/
+
+export CASALD_LIBRARY_PATH=$LD_LIBRARY_PATH
+
+
+# Ensure no time overlap in job start times
+python3 -c "import time, random; time.sleep(random.randint(2, 120))"
+
+export data_path="/home/ekoch/scratch/VLAXL_imaging/MeasurementSets/"
+
+export casa_executable="/home/ekoch/casa-6.6.1-17-pipeline-2024.1.0.8/bin/casa"
+export casa_script="/home/ekoch/lglbs_hi_scripts/fir_imaging/run_lglbs_HI_staging.py"
+# export casa_job_config_file=/home/ekoch/lglbs_hi_scripts/fir_imaging/line_staging_imaging.${SLURM_ARRAY_TASK_ID}.jobconfig.txt
+
+
+# Step 1: Check if we need to untar the MSs
+# Set variable to current working directory
+curr_dir=$(pwd)
+
+cd $data_path
+
+if [ ! -d "$target_name" ]; then
+    echo "No folder $target_name. Restore from project/nearline first with globus"
+fi
+
+cd $target_name
+# Loop through all tar files, untar them, and delete them.
+
+for filename in *.tar; do
+    echo "Untarring $filename"
+    tar -xvf $filename
+    rm $filename
+done
+
+cd $curr_dir
+
+
+# Step 2: Run CASA to do staging.
+xvfb-run -a $casa_executable --rcdir ~/.casa --nologger --nogui --log2term -c $casa_script $target_name
+
---
+++ fir_imaging/run_lglbs_line_staging.py
@@ -0,0 +1,120 @@
+
+'''
+Stage LGLBS line data
+
+Produces staging for:
+- 4 OH lines
+- HI at 4.2 km/s (10x native)
+- HI at 2.1 km/s (5x native)
+- HI at native 0.4 km/s
+- HI at 0.84 km/s
+
+'''
+
+##############################################################################
+# Load routines, initialize handlers
+##############################################################################
+
+import os
+import sys
+
+# Locate the master key
+key_file = "/home/ekoch/lglbs_hi_scripts/lglbs_keys/master_key.txt"
+
+data_path = "/home/ekoch/scratch/VLAXL_imaging/"
+export_path = "/home/ekoch/projects/rrg-eros-ab/ekoch/VLAXL/staged_measurement_sets/"
+
+# Set the logging
+from phangsPipeline import phangsLogger as pl
+# reload(pl)
+pl.setup_logger(level='DEBUG', logfile=None)
+
+from phangsPipeline import handlerKeys as kh
+from phangsPipeline import handlerVis as uvh
+
+
+# Initialize key handlers
+this_kh = kh.KeyHandler(master_key = key_file)
+this_uvh = uvh.VisHandler(key_handler = this_kh)
+
+# Make missing directories
+this_kh.make_missing_directories(imaging=True,derived=True,postprocess=True,release=True)
+
+##############################################################################
+# Set up what we do this run
+##############################################################################
+
+# this_targ = 'm31'
+this_targ = sys.argv[-1].lower()
+
+
+# all_configs = ['C+D', 'A+B+C+D', 'B+C+D', 'A+B+C']
+# all_configs = ['C+D', 'D'] #, 'B+C+D']
+
+all_configs = ['A+B+C+D']
+
+this_uvh.set_targets(only=[this_targ])
+this_uvh.set_interf_configs(only=all_configs)
+
+# all_line_products = ['hi21cm', 'hi21cm_0p8kms', 'hilores', 'himidres',
+#                      'oh1612', 'oh1720', 'oh1665', 'oh1667']
+
+# all_line_products = ['oh1612', 'oh1720', 'oh1665', 'oh1667']
+all_line_products = ['himidres', 'hi21cm_0p8kms', 'hi']
+# all_line_products = ['hilores']
+
+# this_uvh.set_line_products(only=all_line_products)
+
+this_uvh.set_no_cont_products(True)
+
+##############################################################################
+# Run staging
+##############################################################################
+
+for this_line in all_line_products:
+    print(f"Working on {this_line}")
+
+    this_uvh.set_line_products(only=[this_line])
+
+    this_uvh.loop_stage_uvdata(do_copy=True, do_contsub=True,
+                                do_extract_line=False, do_extract_cont=False,
+                                do_remove_staging=False, overwrite=True,
+                                strict_config=False,
+                                statwt_line=False, statwt_cont=False)
+
+    this_uvh.loop_stage_uvdata(do_copy=False, do_contsub=False,
+                                do_extract_line=True, do_extract_cont=False,
+                                do_remove_staging=False, overwrite=True, strict_config=False,
+                                statwt_line=False, statwt_cont=False)
+
+    this_uvh.loop_stage_uvdata(do_copy=False, do_contsub=False,
+                                do_extract_line=False, do_extract_cont=False,
+                                do_remove_staging=True, overwrite=True, strict_config=False,
+                                statwt_line=False, statwt_cont=False)
+
+
+# Now we'll tar up the MS files
+
+# sys.exit(0)
+
+# import tarfile
+
+# for this_config in all_configs:
+#     for this_line in all_line_products:
+
+#         final_msname = f"{this_targ}_{this_config}_{this_line}.ms"
+
+#         full_final_msname = f"{data_path}/imaging/{this_targ}/{final_msname}"
+
+#         if not os.path.exists(full_final_msname):
+#             print(f"Unable to find an existing ms file: {full_final_msname}. Skipping tar")
+#             continue
+
+#         # Create a new tar file. Remove the old version if it already exists.
+#         tar_msname = f"{export_path}/{final_msname}.tar"
+#         if os.path.exists(tar_msname):
+#             print(f"Found existing {tar_msname}. Deleting and creating new tar file.")
+#             os.remove(tar_msname)
+
+#         with tarfile.open(tar_msname, "w") as tfile:
+#             tfile.add(full_final_msname, recursive=True)
